144e62486961402e1e840f1cac7dec43e49d035a
Ana Vasquez <anvasquez08@gmail.com>
2018-04-22 18:33:49 -0400

    Modularized the backend server by including express routers in routes.js and constrollers.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+// Server
 const express = require('express');
-const parser = require('body-parser');
-const axios = require('axios');
-const db = require('../database/index.js');
 const app = express();
-
 app.use(express.static(__dirname + '/../client/dist'));
+
+// Middleware
+const parser = require('body-parser');
 app.use(parser.json());
 
-app.listen(4000, () => console.log('Listening to port 3000')); 
+// MySQL Database
+const db = require('../database/index.js');
+
+// Router 
+const router = require('./routes.js');
+app.use('/myplaces',router);
+
+// Listening and Exporting App
+app.listen(4000, () => console.log('Listening to port 4000')); 
+module.exports.app = app;
