4897236e2a645eb9ffe6aa2a25d18b1871ad3a6a
Ana Vasquez <anvasquez08@gmail.com>
2018-05-11 15:18:54 -0400

    Added additional features.

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,14 +1,7 @@
-import React, { Component } from 'react';
-import Autocomplete from 'react-autocomplete'
+import React from 'react';
 import axios from 'axios';
-import {
-	Container, Row, Col,
-	Card, CardBody, CardTitle, CardSubtitle,CardText,CardGroup,CardImg, CardImgOverlay,
-  Button, 
-	Input,CardDeck
-} from 'reactstrap';
-
-// import sampleData from './sampleData.js';
+import { Col, Card, CardBody, CardTitle, 
+	CardSubtitle, CardText, CardGroup, CardImg, Button } from 'reactstrap';
 
 class Results extends React.Component{
 
@@ -23,14 +16,15 @@ class Results extends React.Component{
 			    this.props.searchResults && 
 			    this.props.searchResults.map((place, idx) => {
 			    	return (
-						 <Col sm="3"key={idx} >
+						 <Col sm="3"key={idx}>
 
 						    <Card style={{width: "18rem"}}>
 						  		<CardImg top width="100%" src={place.image_url} className="img-responsive"/>
 						      <CardBody style={{height: "10rem"}}>
-						          <CardTitle >{place.name}</CardTitle>
-						          <CardSubtitle>{place.rating}</CardSubtitle>
+						          <CardTitle><a href={place.url}>{place.name}</a></CardTitle>
+						          <CardSubtitle>Yelp Rating: {place.rating}</CardSubtitle>
 						          <CardText>{place.location.address1} 
+
 						          <br></br>
 						          {place.location.city}, {place.location.zip_code}</CardText>
 						          <Button>Button</Button>
