81ec8b472c6d8c6658d514fadfb98fa726948fc3
Ana Vasquez <anvasquez08@gmail.com>
2018-05-11 23:55:50 -0400

    Fully implemented result details component

--- client/src/components/RestaurantDetails.jsx
+++ client/src/components/RestaurantDetails.jsx
@@ -7,26 +7,55 @@ class RestaurantDetails extends React.Component {
 
 	render() {
 		return (
-			<div>
-			<Button onClick={() => this.props.handleViewChange()}>Back to search results...</Button>
+			<div style={{ width:" 700px", padding: "20px", border: "5px", margin: "0"}}>
+				<Button onClick={() => this.props.handleViewChange()}>Back to search results...</Button>
+				<Button onClick={() => this.props.handleViewChange()}>Save to Bookmarks</Button>
+				<br></br>
+				<div>
+					<div>
+					<br></br>
 
-			{
-				this.props.yelpResultsForId.map((business, idx) => {
-					return (
-									<div key={idx}>
-										<div >{business.name}</div>
-									  <div>{business.price}</div>
-									 	<div>{business.categories[0].title}</div>
-										<div>{business.display_phone}</div>
-							    	<div>{business.display_address}</div>
-								    <div>{business.rating}</div>
-										<div>{business.reviews.reviews[0].text}</div>
-										<div>{business.reviews.reviews[1].text}</div>
-										<div>{business.reviews.reviews[2].text}</div>
-									</div>
-						)
-				})
-			}
+							{
+								this.props.yelpResultsForId.map((business, idx) => {
+									return (
+										<dl className="row" key={idx}>
+										  <dt className="col-sm-3">Name</dt>
+										  <dd className="col-sm-9">{business.name}</dd>
+
+										  <dt className="col-sm-3">Category</dt>
+										  <dd className="col-sm-9">{business.categories[0].title}</dd>
+
+										  <dt className="col-sm-3">Price</dt>
+										  <dd className="col-sm-9">{business.price}</dd>
+
+										  <dt className="col-sm-3">Phone Number</dt>
+										  <dd className="col-sm-9">{business.display_phone}</dd>
+
+										  <dt className="col-sm-3">Rating</dt>
+										  <dd className="col-sm-9">{business.rating}</dd>
+
+										  <dt className="col-sm-3">Reviews</dt>
+										  <dd className="col-sm-9">
+										    
+										  	<dl className="row">
+											    <dt className="col-sm-4">{business.reviews.reviews[0].user.name}</dt>
+											    <dd className="col-sm-8">{business.reviews.reviews[0].text}</dd>
+										  	</dl>
+											  <dl className="row">
+											    <dt className="col-sm-4">{business.reviews.reviews[1].user.name}</dt>
+											    <dd className="col-sm-8">{business.reviews.reviews[1].text}</dd>
+											  </dl>
+										    <dl className="row">
+										      <dt className="col-sm-4">{business.reviews.reviews[2].user.name}</dt>
+										      <dd className="col-sm-8">{business.reviews.reviews[2].text}</dd>
+										    </dl>
+										  </dd>
+										</dl>
+									)
+								})
+							}
+					</div>
+				</div>
 			</div>
 		)
 	}
