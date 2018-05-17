173c5c1af6eca6be7f2a84fbacc8464e51a4b17f
Ana Vasquez <anvasquez08@gmail.com>
2018-05-17 00:48:52 -0400

    Implemented asynchronous call to nyc health api.

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -3,15 +3,51 @@ const Preferences = sequelize.import(`../database/models/preferences.js`);
 const User = sequelize.import(`../database/models/user.js`);
 const {Headers,Google_API} = require('../config.js')
 const axios = require('axios');
+const Promise = require("bluebird");
+
+const getHeathRating = (singlePlaceObj) => {
+  return axios.get(`https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="${singlePlaceObj.name}"`)
+  .then(response => {
+    if (response.data.length > 0) {
+      singlePlaceObj['healthRating'] = response.data
+      return singlePlaceObj
+    } else {
+      singlePlaceObj['healthRating'] = []
+      return singlePlaceObj
+    }    
+  })
+  .catch(err => console.log('inner error'))
+}
 
 const yelpRoutes= {
+
   searchPlaces: {	
     post: (req, res) => {
       const {lat, lng} = req.body; 
-      axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${50}`, Headers)
-        .then((response) => res.send(response.data))
-        .catch((err) => res.send(err))
+
+
+      axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${4}`, Headers)
+        .then((response) => {   
+          const arrayOfPlaces = response.data.businesses
+
+          Promise.all(arrayOfPlaces.map(getHeathRating)).then(data => console.log(data))
+          res.send('done')
+          // arrayOfPlaces.forEach(obj => {
+          //   getHeathRating(obj).then(response => console.log(response))
+          // })
+
+          // console.log(arrayOfPlaces.forEach(obj => console.log(obj.healthRating)))
+
+        })
+        .catch(err => console.log('outter err'))
+// The Alcove
+// Slide Bar-B-Q
+// Jaew Hon New York
+// Pelicana Chicken
+     
+        
     }
+
   },
   searchId: {
   	post: (req,res) => {
@@ -23,6 +59,8 @@ const yelpRoutes= {
   			.then(response => {
   				const reviews = response.data
           resultDetails['reviews'] = reviews
+          const {name} = resultDetails
+          console.log('this is the name', name)
 	  			res.send(resultDetails)
 	  		 })
   			.catch((err) => console.log('inner error'))
@@ -35,4 +73,60 @@ const yelpRoutes= {
   module.exports = yelpRoutes
   //axios.get(`https://api.yelp.com/v3/businesses/matches?name=${array[0]}&address1=${array[1]}&city=${array[2]}&state=${array[3]}&country=${array[4].slice(0, 2)}`, Headers)
 //https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="Udon West" 
-// https://data.cityofnewyork.us/resource/xx67-kt59.json?$q="Totto Ramen" 
+/*
+//1
+searchPlaces: { 
+    post: (req, res) => {
+      const {lat, lng} = req.body; 
+      axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${50}`, Headers)
+        .then((response) => {
+          const data = response.data
+          // console.log(data)
+          res.send(data)
+
+          Promise.map(data ,function(place) {
+            return  axios.get(`https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="${place.name}"`)
+          }).then(result => {
+             place['healthGrading'] = result
+             console.log('reaching health grading', result)
+             return place
+          }).catch(err => console.log('inner error', err))
+        })
+        .catch(err => console.log('outter err'))
+    }
+
+//2
+
+
+          const resultArray = (arrayOfObjects) => {
+            console.log('result arr')
+            return Promise.each(arrayOfObjects, (singleOnk) => { return getHeathRating(singleOnk) })
+          }
+*/
+
+/*
+  searchPlaces: { 
+    post: (req, res) => {
+      const {lat, lng} = req.body; 
+      axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${4}`, Headers)
+        .then((response) => {
+          const arrayOfPlaces = response.data
+          res.send(arrayOfPlaces)
+          
+          Promise.map(arrayOfPlaces, (place) => {
+            return place
+          })
+          .then(() => console.log('done'))
+          .catch((err) => console.log(err))      
+        })
+        .catch(err => console.log('outter err'))
+
+     
+        
+    }
+    
+  },
+
+
+*/
+
