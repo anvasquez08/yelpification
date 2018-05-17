056293368339ef984dc1d693adcedec3c832c003
Ana Vasquez <anvasquez08@gmail.com>
2018-05-17 01:10:33 -0400

    Refactored function into another file

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const helpers = {
 	googleAddressFormatter: (address, zipcode) => {
 		return 'address='+ address.split(' ').join('+') + '+' + zipcode
@@ -7,8 +9,20 @@ const helpers = {
 			fulladdress: json.data.results[0].formatted_address,
 			coordinates: json.data.results[0].geometry.location
 		}
-	}
+	}, 
+	getHeathRating: (singlePlaceObj) => {
+		return axios.get(`https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="${singlePlaceObj.name}"`)
+		    .then(response => {
+			    if (response.data.length > 0) {
+			      singlePlaceObj['healthRating'] = response.data
+			      return singlePlaceObj
+			    } else {
+			      singlePlaceObj['healthRating'] = []
+			      return singlePlaceObj
+			    }    
+		  })
+		  .catch(err => console.log('inner error', err))
+		}
 }
-
 
 module.exports = helpers
--- controllers/yelp.js
+++ controllers/yelp.js
@@ -1,51 +1,25 @@
 const sequelize = require(`../database/models/index.js`).sequelize;
 const Preferences = sequelize.import(`../database/models/preferences.js`);
 const User = sequelize.import(`../database/models/user.js`);
-const {Headers,Google_API} = require('../config.js')
+const { Headers, Google_API } = require('../config.js')
+let { getHeathRating } = require('./helpers.js')
+
 const axios = require('axios');
 const Promise = require("bluebird");
 
-const getHeathRating = (singlePlaceObj) => {
-  return axios.get(`https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="${singlePlaceObj.name}"`)
-  .then(response => {
-    if (response.data.length > 0) {
-      singlePlaceObj['healthRating'] = response.data
-      return singlePlaceObj
-    } else {
-      singlePlaceObj['healthRating'] = []
-      return singlePlaceObj
-    }    
-  })
-  .catch(err => console.log('inner error'))
-}
-
 const yelpRoutes= {
-
   searchPlaces: {	
     post: (req, res) => {
       const {lat, lng} = req.body; 
-
-
       axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${4}`, Headers)
-        .then((response) => {   
+        .then(response => {   
           const arrayOfPlaces = response.data.businesses
-
-          Promise.all(arrayOfPlaces.map(getHeathRating)).then(data => console.log(data))
-          res.send('done')
-          // arrayOfPlaces.forEach(obj => {
-          //   getHeathRating(obj).then(response => console.log(response))
-          // })
-
-          // console.log(arrayOfPlaces.forEach(obj => console.log(obj.healthRating)))
+          Promise.all(arrayOfPlaces.map(getHeathRating))
+          .then(data => res.send(data))
+          .catch(err => console.log(err))
 
         })
-        .catch(err => console.log('outter err'))
-// The Alcove
-// Slide Bar-B-Q
-// Jaew Hon New York
-// Pelicana Chicken
-     
-        
+        .catch(err => console.log('outter err'))       
     }
 
   },
@@ -70,63 +44,21 @@ const yelpRoutes= {
   }
 }
 
-  module.exports = yelpRoutes
+module.exports = yelpRoutes
+
+// const getHeathRating = (singlePlaceObj) => {
+//   return axios.get(`https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="${singlePlaceObj.name}"`)
+//   .then(response => {
+//     if (response.data.length > 0) {
+//       singlePlaceObj['healthRating'] = response.data
+//       return singlePlaceObj
+//     } else {
+//       singlePlaceObj['healthRating'] = []
+//       return singlePlaceObj
+//     }    
+//   })
+//   .catch(err => console.log('inner error'))
+// }
+
   //axios.get(`https://api.yelp.com/v3/businesses/matches?name=${array[0]}&address1=${array[1]}&city=${array[2]}&state=${array[3]}&country=${array[4].slice(0, 2)}`, Headers)
 //https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="Udon West" 
-/*
-//1
-searchPlaces: { 
-    post: (req, res) => {
-      const {lat, lng} = req.body; 
-      axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${50}`, Headers)
-        .then((response) => {
-          const data = response.data
-          // console.log(data)
-          res.send(data)
-
-          Promise.map(data ,function(place) {
-            return  axios.get(`https://data.cityofnewyork.us/resource/xx67-kt59.json?$where=dba="${place.name}"`)
-          }).then(result => {
-             place['healthGrading'] = result
-             console.log('reaching health grading', result)
-             return place
-          }).catch(err => console.log('inner error', err))
-        })
-        .catch(err => console.log('outter err'))
-    }
-
-//2
-
-
-          const resultArray = (arrayOfObjects) => {
-            console.log('result arr')
-            return Promise.each(arrayOfObjects, (singleOnk) => { return getHeathRating(singleOnk) })
-          }
-*/
-
-/*
-  searchPlaces: { 
-    post: (req, res) => {
-      const {lat, lng} = req.body; 
-      axios.get(`https://api.yelp.com/v3/businesses/search?term="food"&latitude=${lat}&longitude=${lng}&open_now=${true}&limit=${4}`, Headers)
-        .then((response) => {
-          const arrayOfPlaces = response.data
-          res.send(arrayOfPlaces)
-          
-          Promise.map(arrayOfPlaces, (place) => {
-            return place
-          })
-          .then(() => console.log('done'))
-          .catch((err) => console.log(err))      
-        })
-        .catch(err => console.log('outter err'))
-
-     
-        
-    }
-    
-  },
-
-
-*/
-
