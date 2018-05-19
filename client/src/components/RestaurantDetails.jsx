52d72410f8d37d99138dfb43dfd2eaab15f036b0
Ana Vasquez <anvasquez08@gmail.com>
2018-05-19 12:15:28 -0400

    Business profile comonent is fully functioning

--- client/src/components/RestaurantDetails.jsx
+++ client/src/components/RestaurantDetails.jsx
@@ -9,33 +9,29 @@ class RestaurantDetails extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			Y2015: [],
-			Y2016: [],
-			Y2017: [],
-			Y2018: []
+			yearsAvailable: [],
+			yearToView:[]
 		}
+		this.handleYearChange = this.handleYearChange.bind(this);
 	}
 
 	componentDidMount() {
-		let d1 = this.props.yelpResultsForId[0].healthRating[0]['2015']
-		this.setState({Y2015: d1})
-		let d2 = this.props.yelpResultsForId[0].healthRating[0]['2016']
-		this.setState({Y2016: d2})
-		let d3 = this.props.yelpResultsForId[0].healthRating[0]['2017']
-		this.setState({Y2017: d3})
-		let d4 = this.props.yelpResultsForId[0].healthRating[0]['2018']
-		this.setState({Y2018: d4})
+		const years = Object.keys(this.props.yelpResultsForId[0].healthRating[0])		
+		this.setState({yearsAvailable: years}, () => {
+			  const dataToDisplay = this.props.yelpResultsForId[0].healthRating[0][this.state.yearsAvailable[this.state.yearsAvailable.length - 1]]
+				this.setState({yearToView: dataToDisplay})
+		})
 	}
 
-	viewChanger() {
-
+	handleYearChange(data) {
+		const dataToDisplay = this.props.yelpResultsForId[0].healthRating[0][data]
+		this.setState({yearToView: dataToDisplay})  
 	}
 
 	render() {
 		const place = this.props.yelpResultsForId[0]
 		const name = place.name	
 		const address = place.location.display_address.join(', ')
-		const {Y2015, Y2016, Y2017, Y2018} = this.state
 
 		return (
 				<div className="container-fluid">
@@ -46,16 +42,16 @@ class RestaurantDetails extends React.Component {
 							
 							<div className="row" style={{fontSize: '15px'}}>
 								<Information yelpResultsForId={this.props.yelpResultsForId}/>
-											<div className="btn-group btn-group-sm" role="group" aria-label="Basic example" style={{float: "right", padding: '5px'}}>
-											{ Y2018 && <button type="button" className="btn btn-secondary">2018</button> }
-											{ Y2017 && <button type="button" className="btn btn-secondary">2017</button> }
-											{ Y2016 && <button type="button" className="btn btn-secondary">2016</button> }
-											{ Y2015 && <button type="button" className="btn btn-secondary">2015</button> }
-											</div>
 
-								<Chart year={this.state.Y2018}/>
-
-							</div>
+									<div className="btn-group btn-group-sm" role="group" aria-label="Basic example" style={{float: "right", padding: '5px'}}>
+										{
+											this.state.yearsAvailable.map((year, idx) => {
+												return (<button type="button" className="btn btn-secondary" onClick={() => this.handleYearChange(year)} key={idx}>{year}</button>)
+											})
+										}
+									</div>
+								<Chart year={this.state.yearToView}/> 
+						</div>
 
 							<Button className="Button-details" onClick={() => this.props.savePlaceToDB(business)}>Save to Bookmarks</Button>
 							<Button className="Button-details" onClick={() => this.props.handleViewStateChange('restaurantResults')}>Back to search results...</Button>		
@@ -66,11 +62,3 @@ class RestaurantDetails extends React.Component {
 }
 
 export default RestaurantDetails;
-
-// Restaurant received an A inspection grade (i.e. a grade of 13 points or below)
-// restaurant received 14 or more violation points (the equivalent of a "B" or worse) during an initial un-graded inspection or during a follow-up inspection, in which case the restaurant would likely display a grade-pending card, instead of a "B" or "C". 
-
-
-
-
-
